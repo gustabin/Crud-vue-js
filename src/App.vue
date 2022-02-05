a6b2490dcd15f526b6cb4b8ac00e66e7be64a4a6
gustabin <gustabin@yahoo.com>
2022-02-05 19:55:46 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="nav navbar-nav">
+      <router-link to="/crear" class="nav-link">Crear</router-link>
+      <router-link to="/listar" class="nav-link">Empleados</router-link>
+    </div>
+  </nav>
+  <br />
+  <router-view />
+</template>
---
+++ src/components/Editar.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="card-header">Actualizar registro</div>
+      <div class="card-body">
+        <form v-on:submit.prevent="actualizarRegistro">
+          <div class="mb-3">
+            <label for="nombre" class="form-label">Nombre:</label>
+            <input
+              type="text"
+              class="form-control"
+              name="nombre"
+              id="nombre"
+              aria-describedby="helpId"
+              placeholder="Nombre"
+              v-model="empleado.nombre"
+              required
+            />
+            <small id="helpId" class="form-text text-muted"
+              >Escribe el nombre del empleado</small
+            >
+          </div>
+          <div class="mb-3">
+            <label for="correo" class="form-label">Correo:</label>
+            <input
+              type="email"
+              class="form-control"
+              name="correo"
+              id="correo"
+              aria-describedby="helpId"
+              placeholder="Correo"
+              v-model="empleado.correo"
+              required
+            />
+            <small id="helpId" class="form-text text-muted"
+              >Escribe el correo del empleado</small
+            >
+          </div>
+          <div class="btn-group" role="group" aria-label="">
+            <button type="submit" class="btn btn-success">Modificar</button
+            >&nbsp;
+            <router-link :to="{ name: 'Listar' }" class="btn btn-warning"
+              >Cancelar</router-link
+            >
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      empleado: {},
+    };
+  },
+  created: function () {
+    this.obtenerInformacionId();
+  },
+  methods: {
+    obtenerInformacionId() {
+      fetch("http://localhost/empleados/?consultar=" + this.$route.params.id)
+        .then((respuesta) => respuesta.json())
+        .then((datosRespuesta) => {
+          console.log(datosRespuesta);
+          this.empleado = datosRespuesta[0];
+          //   window.location.href = "listar";
+        })
+        .catch(console.log);
+    },
+    actualizarRegistro() {
+      var datosEnviar = {
+        id: this.$route.params.id,
+        nombre: this.empleado.nombre,
+        correo: this.empleado.correo,
+      };
+      fetch("http://localhost/empleados/?actualizar=" + this.$route.params.id, {
+        method: "POST",
+        body: JSON.stringify(datosEnviar),
+      })
+        .then((respuesta) => respuesta.json())
+        .then((datosRespuesta) => {
+          console.log(datosRespuesta);
+          window.location.href = "../listar";
+        });
+    },
+  },
+};
+</script>
